9624ef3847338b85373bedff17b3ed5c86014979
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 05:46:29 +0200

    Add new testIfConstructorIsInitializeValid in AccountTest class

--- src/test/java/com/codecoolbank/javase/model/AccountTest.java
+++ src/test/java/com/codecoolbank/javase/model/AccountTest.java
@@ -16,4 +16,14 @@ class AccountTest {
         Account account = new Account(customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 1);
         assertEquals(Account.class, account.getClass());
     }
+
+    @Test
+    void testIfConstructorIsInitializedValid() {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 2,5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        Account account = new Account(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 1);
+        assertEquals(Account.class, account.getClass());
+    }
 }
