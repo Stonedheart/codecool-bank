4395b65d692817781476314b82ca86518a03e511
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 18:10:20 +0200

    Add tests to check if DebitLimit and Interest is valid in DebitAccountTest class.

--- src/test/java/com/codecoolbank/javase/model/DebitAccountTest.java
+++ src/test/java/com/codecoolbank/javase/model/DebitAccountTest.java
@@ -89,4 +89,24 @@ class DebitAccountTest {
         BigDecimal newBalance = BigDecimal.valueOf(-1.00);
         assertThrows(InvalidValue.class, ()-> new DebitAccount(debitAccount.getId(), debitAccount.getCustomer(), debitAccount.getNumber(), debitAccount.getAccountType(), debitAccount.getAccountStatus(), debitAccount.getOpenDate(), newBalance, debitAccount.getDebitLine(), debitAccount.getInterest()));
     }
+
+    @Test
+    void testIfDebitLineFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        DebitAccount debitAccount = new DebitAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(BigDecimal.valueOf(0.00), debitAccount.getDebitLine());
+    }
+
+    @Test
+    void testIfInterestFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        DebitAccount debitAccount = new DebitAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(5, debitAccount.getInterest().intValue());
+    }
 }
