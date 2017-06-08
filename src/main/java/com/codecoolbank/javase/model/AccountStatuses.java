e00af48b46abdbc66ee6622aab5b5ade7e4f37ef
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:22:39 +0200

    Add new constructor without ID in AccountStatuses class

--- src/main/java/com/codecoolbank/javase/model/AccountStatuses.java
+++ src/main/java/com/codecoolbank/javase/model/AccountStatuses.java
@@ -2,6 +2,10 @@ package com.codecoolbank.javase.model;
 
 public class AccountStatuses extends CodecoolBankModel {
 
+    public AccountStatuses(String name, String description) {
+        super(name, description);
+    }
+
     public AccountStatuses(Integer id, String name, String description) {
         super(id, name, description);
     }
