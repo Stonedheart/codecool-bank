1161c85e3b35b2be4f97d1cbfd0e1533853b6523
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 04:25:21 +0200

    Declare private fields for Cards class

--- src/main/java/com/codecoolbank/javase/model/Cards.java
+++ src/main/java/com/codecoolbank/javase/model/Cards.java
@@ -1,4 +1,14 @@
 package com.codecoolbank.javase.model;
 
+import java.math.BigDecimal;
+
 public class Cards {
+    private String number;
+    private String validity;
+    private BigDecimal buyingLimit;
+    private BigDecimal cashWithDrawLimit;
+    private BigDecimal limit;
+    private CardTypes cardType;
+    private CardStatuses cardStatus;
+    private Accounts account;
 }
