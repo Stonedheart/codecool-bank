5b6138e5539e956b40e6790e3fe9bb14212e8692
Stonedheart <tytomaszto@gmail.com>
2017-06-08 09:53:12 +0200

    Add login tests in CustomerDaoTest

--- src/test/java/com/codecoolbank/javase/dao/CustomerDaoImplTest.java
+++ src/test/java/com/codecoolbank/javase/dao/CustomerDaoImplTest.java
@@ -11,6 +11,23 @@ import static org.junit.jupiter.api.Assertions.*;
 class CustomerDaoImplTest {
 
     @Test
+    void testIfSuccessfullLogInReturnTrue() throws SQLException {
+        Customer testCustomer = new Customer(1,"Jan", "Kowalski", "jfk", "notdead", "11-02-2014", true, "06-06-2017");
+        CustomerDaoImpl testCustomerDao = new CustomerDaoImpl();
+        String validLogin = testCustomer.getLogin();
+        String validPassword = testCustomer.getPassword();
+        assertTrue(testCustomerDao.logIn(validLogin, validPassword));
+    }
+
+    @Test
+    void testIfFailLogInReturnFalse() throws SQLException {
+        CustomerDaoImpl testCustomerDao = new CustomerDaoImpl();
+        String invalidLogin = "user";
+        String invalidPassword = "1234";
+        assertFalse(testCustomerDao.logIn(invalidLogin, invalidPassword));
+    }
+
+    @Test
     void testIfCustomerIsInsertToDatabaseProperly() throws SQLException {
         Customer testCustomer = new Customer("Bob", "Nowak", "urMum", "notDead", "11-11-2014", true, "24-01-2017");
         CustomerDaoImpl customerDao = new CustomerDaoImpl();
