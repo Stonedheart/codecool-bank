9bebe8bb974afbbd15cda21142cd5713fbb01ca3
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 04:49:16 +0200

    Changeorder of attrubutes in Transactions class

--- src/main/java/com/codecoolbank/javase/model/Transactions.java
+++ src/main/java/com/codecoolbank/javase/model/Transactions.java
@@ -4,27 +4,27 @@ import java.math.BigDecimal;
 
 public class Transactions extends AbstractCodecoolBankModel {
     private String dateOfTransaction;
-    private BigDecimal value;
     private TransactionTypes transactionType;
+    private BigDecimal value;
     private TransactionStatuses transactionStatus;
     private Accounts account;
     private Cards card;
 
-    public Transactions(String dateOfTransaction, BigDecimal value, String description, TransactionTypes transactionType, TransactionStatuses transactionStatus, Accounts account, Cards card) {
+    public Transactions(String dateOfTransaction, TransactionTypes transactionType, BigDecimal value, String description, TransactionStatuses transactionStatus, Accounts account, Cards card) {
         super(description);
         this.dateOfTransaction = dateOfTransaction;
-        this.value = value;
         this.transactionType = transactionType;
+        this.value = value;
         this.transactionStatus = transactionStatus;
         this.account = account;
         this.card = card;
     }
 
-    public Transactions(Integer id, String dateOfTransaction, BigDecimal value, String description, TransactionTypes transactionType, TransactionStatuses transactionStatus, Accounts account, Cards card) {
+    public Transactions(Integer id, String dateOfTransaction, TransactionTypes transactionType, BigDecimal value, String description, TransactionStatuses transactionStatus, Accounts account, Cards card) {
         super(id, description);
         this.dateOfTransaction = dateOfTransaction;
-        this.value = value;
         this.transactionType = transactionType;
+        this.value = value;
         this.transactionStatus = transactionStatus;
         this.account = account;
         this.card = card;
