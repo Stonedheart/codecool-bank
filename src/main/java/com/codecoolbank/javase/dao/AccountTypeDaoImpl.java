d5be31bd6d39916e1f9bc239aa035132f34feb27
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 13:37:51 +0200

    Initialize AccountTypeDaoImpl class

---
+++ src/main/java/com/codecoolbank/javase/dao/AccountTypeDaoImpl.java
@@ -0,0 +1,12 @@
+package com.codecoolbank.javase.dao;
+
+import com.codecoolbank.javase.model.AccountType;
+
+import java.sql.SQLException;
+
+public class AccountTypeDaoImpl implements AccountTypeDao {
+
+    @Override
+    public AccountType findAccountTypeById(Integer id) throws SQLException {
+    }
+}
