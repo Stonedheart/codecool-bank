8d6e6d4e1b944acc2d377190ef4a79f01d1b6676
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 10:40:29 +0200

    Add implementation of AccountController interface class

---
+++ src/main/java/com/codecoolbank/javase/controller/AccountControllerImpl.java
@@ -0,0 +1,23 @@
+package com.codecoolbank.javase.controller;
+
+public class AccountControllerImpl implements AccountController {
+    @Override
+    public void deposit(long amount) {
+
+    }
+
+    @Override
+    public void withdraw(long amount) {
+
+    }
+
+    @Override
+    public long getBalance() {
+        return 0;
+    }
+
+    @Override
+    public int getAccountId() {
+        return 0;
+    }
+}
