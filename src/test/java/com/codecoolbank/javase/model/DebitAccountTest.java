7796f7ae9371600e0e5cb64cd8e578cede0c8137
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 17:02:23 +0200

    Add test to check if AccountStatus is valid in constructor.

--- src/test/java/com/codecoolbank/javase/model/DebitAccountTest.java
+++ src/test/java/com/codecoolbank/javase/model/DebitAccountTest.java
@@ -50,6 +50,16 @@ class DebitAccountTest {
     }
 
     @Test
+    void testIfAccountStatusFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        DebitAccount debitAccount = new DebitAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(accountStatus.getId(), debitAccount.getAccountStatus().getId());
+    }
+
+    @Test
     void testIfBalanceFromDebitAccountIsValid() throws InvalidValue {
         Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
         AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
