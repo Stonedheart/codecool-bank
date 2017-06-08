79b726b824adf7ee92fce6bc83a8d6a127877fe0
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 06:25:30 +0200

    Rename class and constructors from Account to AccountImpl

--- src/main/java/com/codecoolbank/javase/model/AccountImpl.java
+++ src/main/java/com/codecoolbank/javase/model/AccountImpl.java
@@ -2,7 +2,7 @@ package com.codecoolbank.javase.model;
 
 import java.math.BigDecimal;
 
-public class Account extends AbstractNumberModel {
+public class AccountImpl extends AbstractNumberModel {
     private Customer customer;
     private AccountType accountType;
     private AccountStatus accountStatus;
@@ -11,7 +11,7 @@ public class Account extends AbstractNumberModel {
     private BigDecimal debitLine;
     private Integer interest;
 
-    public Account(Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
+    public AccountImpl(Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
         super(number);
         this.customer = customer;
         this.accountType = accountType;
@@ -22,7 +22,7 @@ public class Account extends AbstractNumberModel {
         this.interest = interest;
     }
 
-    public Account(Integer id, Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
+    public AccountImpl(Integer id, Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
         super(id, number);
         this.customer = customer;
         this.accountType = accountType;
