67b03e567436f8361f7de9890c814ca29d5bbb72
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 04:01:11 +0200

    Add constructor without ID in Transactions class

--- src/main/java/com/codecoolbank/javase/model/Transactions.java
+++ src/main/java/com/codecoolbank/javase/model/Transactions.java
@@ -10,4 +10,14 @@ public class Transactions {
     private TransactionStatuses transactionStatus;
     private Account account;
     private Card card;
+
+    public Transactions(String dateOfTransaction, BigDecimal value, String description, TransactionTypes transactionType, TransactionStatuses transactionStatus, Account account, Card card) {
+        this.dateOfTransaction = dateOfTransaction;
+        this.value = value;
+        this.description = description;
+        this.transactionType = transactionType;
+        this.transactionStatus = transactionStatus;
+        this.account = account;
+        this.card = card;
+    }
 }
