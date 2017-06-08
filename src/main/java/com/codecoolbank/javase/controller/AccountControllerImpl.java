cca13cfcb5b402ad44b771970eded0feec50f54c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 14:29:15 +0200

    Handle exception if Account is null in AccountControllerImpl

--- src/main/java/com/codecoolbank/javase/controller/AccountControllerImpl.java
+++ src/main/java/com/codecoolbank/javase/controller/AccountControllerImpl.java
@@ -1,9 +1,20 @@
 package com.codecoolbank.javase.controller;
 
+import com.codecoolbank.javase.dao.AccountDaoImpl;
+import com.codecoolbank.javase.model.Account;
+import org.omg.CORBA.DynAnyPackage.InvalidValue;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
 public class AccountControllerImpl implements AccountController {
 
     @Override
-    public void deposit(long amount) {
+    public void deposit(Integer id, BigDecimal amount) throws SQLException, InvalidValue {
+        Account account = new AccountDaoImpl().findAccountByID(id);
+        if (account.equals(null)) {
+            throw new NullPointerException("Account does not exists!");
+        }
 
     }
 
