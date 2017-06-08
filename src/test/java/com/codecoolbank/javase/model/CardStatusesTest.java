ef1a0421bb22d73d0364c83a4828fd87b85f2e6f
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 03:12:49 +0200

    Add testIfConstructorWithoutIDIsInitializedValid in CardStatusesTest class

--- src/test/java/com/codecoolbank/javase/model/CardStatusesTest.java
+++ src/test/java/com/codecoolbank/javase/model/CardStatusesTest.java
@@ -1,7 +1,13 @@
 package com.codecoolbank.javase.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CardStatusesTest {
-
+    @Test
+    void testIfConstructorWithoutIDIsInitializedValid() {
+        CardStatuses accountStatuses = new CardStatuses("Active","Card is active");
+        assertEquals(CardStatuses.class, accountStatuses.getClass());
+    }
 }
