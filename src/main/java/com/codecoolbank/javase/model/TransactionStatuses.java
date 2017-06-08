1e9b1e5e001f6789c5153a42212f4e59f5689eea
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 03:36:56 +0200

    Add new constructor with ID in TransactionStatuses class

--- src/main/java/com/codecoolbank/javase/model/TransactionStatuses.java
+++ src/main/java/com/codecoolbank/javase/model/TransactionStatuses.java
@@ -5,4 +5,8 @@ public class TransactionStatuses extends AbstractCodecoolBankModel {
     public TransactionStatuses(String name, String description) {
         super(name, description);
     }
+
+    public TransactionStatuses(Integer id, String name, String description) {
+        super(id, name, description);
+    }
 }
