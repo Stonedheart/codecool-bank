d3622f96894acae58ae2a2fdf63ee8813c3bab7c
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 17:06:18 +0200

    Add test to check if Customer, AccountType, AccountStatus and OpenDate is valid in constructor.

--- src/test/java/com/codecoolbank/javase/model/SavingAccountTest.java
+++ src/test/java/com/codecoolbank/javase/model/SavingAccountTest.java
@@ -29,6 +29,46 @@ class SavingAccountTest {
     }
 
     @Test
+    void testIfCustomerFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        SavingAccount savingAccount = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(customer.getId(), savingAccount.getCustomer().getId());
+    }
+
+    @Test
+    void testIfAccountTypeFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        SavingAccount savingAccount = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(accountType.getId(), savingAccount.getAccountType().getId());
+    }
+
+    @Test
+    void testIfAccountStatusFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        SavingAccount savingAccount = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(accountStatus.getId(), savingAccount.getAccountStatus().getId());
+    }
+
+    @Test
+    void testIfOpenDateFromDebitAccountIsValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        SavingAccount savingAccount = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals("12-02-2015", savingAccount.getOpenDate());
+    }
+
+    @Test
     void testIfBalanceFromSavingAccountIsValid() throws InvalidValue {
         Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
         AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
