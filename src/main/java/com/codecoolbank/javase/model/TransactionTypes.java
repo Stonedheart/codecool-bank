da969c79ca725fe4bc9b5b2869e716aa0e46497d
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 03:24:39 +0200

    Add constructor with ID in TransactionTypes class

--- src/main/java/com/codecoolbank/javase/model/TransactionTypes.java
+++ src/main/java/com/codecoolbank/javase/model/TransactionTypes.java
@@ -5,4 +5,8 @@ public class TransactionTypes extends AbstractCodecoolBankModel {
     public TransactionTypes(String name, String description) {
         super(name, description);
     }
+
+    public TransactionTypes(Integer id, String name, String description) {
+        super(id, name, description);
+    }
 }
