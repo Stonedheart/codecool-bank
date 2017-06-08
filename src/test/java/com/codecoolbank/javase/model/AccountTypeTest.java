6c1438950a851529e540a2a9336d8c93be7c907d
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 05:38:03 +0200

    Change instance name in AccountTypeTest class

--- src/test/java/com/codecoolbank/javase/model/AccountTypeTest.java
+++ src/test/java/com/codecoolbank/javase/model/AccountTypeTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class AccountTypeTest {
     @Test
     void testIfConstructorWithoutIDIsInitializedValid() {
-        AccountType testAccountType = new AccountType("Saving account", "Account for your savings, percentage 2,5%");
-        assertEquals(AccountType.class, testAccountType.getClass());
+        AccountType accountType = new AccountType("Saving account", "Account for your savings, percentage 2,5%");
+        assertEquals(AccountType.class, accountType.getClass());
     }
 
     @Test
     void testIfConstructorIsInitializedValid() {
-        AccountType testAccountType = new AccountType(1, "Saving account", "Account for your savings, percentage 2,5%");
-        assertEquals(AccountType.class, testAccountType.getClass());
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 2,5%");
+        assertEquals(AccountType.class, accountType.getClass());
     }
 }
