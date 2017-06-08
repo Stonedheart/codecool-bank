6e1dd01b97d7fe653cfbabe5e68753f26a8da99f
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:37:39 +0200

    Add new testIfConstructorWithoutIDIsInitializedValid in AccountTypesTest class

--- src/test/java/com/codecoolbank/javase/model/AccountTypesTest.java
+++ src/test/java/com/codecoolbank/javase/model/AccountTypesTest.java
@@ -1,7 +1,13 @@
 package com.codecoolbank.javase.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountTypesTest {
-
+    @Test
+    void testIfConstructorWithoutIDIsInitializedValid() {
+        AccountTypes accountStatuses = new AccountTypes("Debit","Account for your savings, without credit");
+        assertEquals(AccountTypes.class, accountStatuses.getClass());
+    }
 }
