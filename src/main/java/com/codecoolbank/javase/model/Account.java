056f12d9ec303a27525e22a0358942ac24c01c39
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 10:38:45 +0200

    Delete implementation of unused interface

--- src/main/java/com/codecoolbank/javase/model/Account.java
+++ src/main/java/com/codecoolbank/javase/model/Account.java
@@ -2,7 +2,7 @@ package com.codecoolbank.javase.model;
 
 import java.math.BigDecimal;
 
-public abstract class AbstractAccount extends AbstractNumberModel {
+public abstract class Account extends AbstractNumberModel {
     private Customer customer;
     private AccountType accountType;
     private AccountStatus accountStatus;
@@ -11,7 +11,7 @@ public abstract class AbstractAccount extends AbstractNumberModel {
     private BigDecimal debitLine;
     private Integer interest;
 
-    public AbstractAccount(Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
+    public Account(Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
         super(number);
         this.customer = customer;
         this.accountType = accountType;
@@ -22,7 +22,7 @@ public abstract class AbstractAccount extends AbstractNumberModel {
         this.interest = interest;
     }
 
-    public AbstractAccount(Integer id, Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
+    public Account(Integer id, Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
         super(id, number);
         this.customer = customer;
         this.accountType = accountType;
--- src/main/java/com/codecoolbank/javase/model/SavingAccount.java
+++ src/main/java/com/codecoolbank/javase/model/SavingAccount.java
@@ -2,7 +2,7 @@ package com.codecoolbank.javase.model;
 
 import java.math.BigDecimal;
 
-public class SavingAccount extends AbstractAccount {
+public class SavingAccount extends Account {
 
     public SavingAccount(Customer customer, String number, AccountType accountType, AccountStatus accountStatus, String openDate, BigDecimal balance, BigDecimal debitLine, Integer interest) {
         super(customer, number, accountType, accountStatus, openDate, balance, debitLine, interest);
