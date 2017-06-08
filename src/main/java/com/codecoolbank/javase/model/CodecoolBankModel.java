af7d1eda43818f57935a5847b0921067e1565de6
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:20:42 +0200

    Add new constructor without id in CodecoolBankModel abstract class

--- src/main/java/com/codecoolbank/javase/model/CodecoolBankModel.java
+++ src/main/java/com/codecoolbank/javase/model/CodecoolBankModel.java
@@ -4,6 +4,11 @@ abstract class CodecoolBankModel extends BaseModel {
     private String name;
     private String description;
 
+    public CodecoolBankModel(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
     public CodecoolBankModel(Integer id, String name, String description) {
         super(id);
         this.name = name;
