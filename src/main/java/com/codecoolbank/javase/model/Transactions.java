ee7d3e3aad318aced399ccaa618073e2f462f1cf
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 04:09:13 +0200

    Add new constructor with id, apply inheritance from AbstractCodecoolBankModel in both constructors

--- src/main/java/com/codecoolbank/javase/model/Transactions.java
+++ src/main/java/com/codecoolbank/javase/model/Transactions.java
@@ -2,19 +2,28 @@ package com.codecoolbank.javase.model;
 
 import java.math.BigDecimal;
 
-public class Transactions {
+public class Transactions extends AbstractCodecoolBankModel {
     private String dateOfTransaction;
     private BigDecimal value;
-    private String description;
     private TransactionTypes transactionType;
     private TransactionStatuses transactionStatus;
     private Account account;
     private Card card;
 
     public Transactions(String dateOfTransaction, BigDecimal value, String description, TransactionTypes transactionType, TransactionStatuses transactionStatus, Account account, Card card) {
+        super(description);
         this.dateOfTransaction = dateOfTransaction;
         this.value = value;
-        this.description = description;
+        this.transactionType = transactionType;
+        this.transactionStatus = transactionStatus;
+        this.account = account;
+        this.card = card;
+    }
+
+    public Transactions(Integer id, String dateOfTransaction, BigDecimal value, String description, TransactionTypes transactionType, TransactionStatuses transactionStatus, Account account, Card card) {
+        super(id, description);
+        this.dateOfTransaction = dateOfTransaction;
+        this.value = value;
         this.transactionType = transactionType;
         this.transactionStatus = transactionStatus;
         this.account = account;
