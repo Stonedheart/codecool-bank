15b9316a46ca12ba6f87cc968d5acfe78db43ed1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 04:44:23 +0200

    Change order of attributes in Cards class

--- src/main/java/com/codecoolbank/javase/model/Cards.java
+++ src/main/java/com/codecoolbank/javase/model/Cards.java
@@ -4,34 +4,34 @@ import java.math.BigDecimal;
 
 public class Cards extends AbstractBaseModel {
     private String number;
+    private CardTypes cardType;
     private String validity;
     private BigDecimal buyingLimit;
     private BigDecimal cashWithDrawLimit;
     private BigDecimal limit;
-    private CardTypes cardType;
-    private CardStatuses cardStatus;
     private Accounts account;
+    private CardStatuses cardStatus;
 
-    public Cards(String number, String validity, BigDecimal buyingLimit, BigDecimal cashWithDrawLimit, BigDecimal limit, CardTypes cardType, CardStatuses cardStatus, Accounts account) {
+    public Cards(String number, CardTypes cardType, String validity, BigDecimal buyingLimit, BigDecimal cashWithDrawLimit, BigDecimal limit, Accounts account, CardStatuses cardStatus) {
         this.number = number;
+        this.cardType = cardType;
         this.validity = validity;
         this.buyingLimit = buyingLimit;
         this.cashWithDrawLimit = cashWithDrawLimit;
         this.limit = limit;
-        this.cardType = cardType;
-        this.cardStatus = cardStatus;
         this.account = account;
+        this.cardStatus = cardStatus;
     }
 
-    public Cards(Integer id, String number, String validity, BigDecimal buyingLimit, BigDecimal cashWithDrawLimit, BigDecimal limit, CardTypes cardType, CardStatuses cardStatus, Accounts account) {
+    public Cards(Integer id, String number, CardTypes cardType, String validity, BigDecimal buyingLimit, BigDecimal cashWithDrawLimit, BigDecimal limit, Accounts account, CardStatuses cardStatus) {
         super(id);
         this.number = number;
+        this.cardType = cardType;
         this.validity = validity;
         this.buyingLimit = buyingLimit;
         this.cashWithDrawLimit = cashWithDrawLimit;
         this.limit = limit;
-        this.cardType = cardType;
-        this.cardStatus = cardStatus;
         this.account = account;
+        this.cardStatus = cardStatus;
     }
 }
