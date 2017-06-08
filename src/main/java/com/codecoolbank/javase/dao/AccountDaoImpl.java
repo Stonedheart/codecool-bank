0d35c7a50cbf60189d3b0dc4799d15598da3cfd3
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 13:03:32 +0200

    Add findByID() method in AccountDaoImpl class

--- src/main/java/com/codecoolbank/javase/dao/AccountDaoImpl.java
+++ src/main/java/com/codecoolbank/javase/dao/AccountDaoImpl.java
@@ -1,4 +1,50 @@
 package com.codecoolbank.javase.dao;
 
-public class AccountDaoImpl {
+import com.codecoolbank.javase.model.Account;
+import com.codecoolbank.javase.model.AccountType;
+import com.codecoolbank.javase.model.Customer;
+import com.codecoolbank.javase.model.SavingAccount;
+import com.oracle.jrockit.jfr.InvalidValueException;
+import org.omg.CORBA.DynAnyPackage.InvalidValue;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class AccountDaoImpl implements AccountDao {
+    SQLiteJDBC connToDB = SQLiteJDBC.getInstance();
+
+    @Override
+    public Account findByID(Integer id) throws SQLException, InvalidValue {
+        String selectQuery = String.format("SELECT * FROM Accounts WHERE `AccountID` = ?");
+        PreparedStatement preparedStatement = connToDB.connectionWithDB().prepareStatement(selectQuery);
+        preparedStatement.setInt(1, id);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (!resultSet.isClosed()) {
+            id = resultSet.getInt("AccountID");
+            Integer customerID = resultSet.getInt("CustomerID");
+            String number = resultSet.getString("Number");
+            Integer accountTypeID = resultSet.getInt("AccountTypeID");
+            Integer accountStatusID = resultSet.getInt("AccountStatusID");
+            String openDate = resultSet.getString("OpenDate");
+            Integer balance = resultSet.getInt("Balance");
+            Integer debitLine = resultSet.getInt("DebitLine");
+            Integer interest = resultSet.getInt("Interest");
+
+            preparedStatement.close();
+
+            switch (accountTypeID) {
+                case 1:
+                    return new SavingAccount(id, customerID, number, accountTypeID, accountStatusID, openDate, balance, debitLine, interest);
+                case 2:
+                    return new DebitAccount(id, customerID, number, accountTypeID, accountStatusID, openDate, balance, debitLine, interest);
+                default:
+                    throw new InvalidValue("There is no such account type!");
+            }
+
+        } else {
+            return null;
+        }
+    }
 }
