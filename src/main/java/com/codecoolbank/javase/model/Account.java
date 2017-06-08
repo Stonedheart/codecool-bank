00a7884bc482c039379c00d46ac49a048fe8f810
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 06:35:54 +0200

    Fill the Account interface by abstract methods

--- src/main/java/com/codecoolbank/javase/model/Account.java
+++ src/main/java/com/codecoolbank/javase/model/Account.java
@@ -1,4 +1,8 @@
 package com.codecoolbank.javase.model;
 
 public interface Account {
+    public void deposit(long amount);
+    public void withdraw(long amount);
+    public long getBalance();
+    public int getAccountId();
 }
