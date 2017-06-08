27e4e0f2062ce2a8ac65ff3394552995a8be7762
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 03:58:20 +0200

    Add private fields to Transactions class

--- src/main/java/com/codecoolbank/javase/model/Transactions.java
+++ src/main/java/com/codecoolbank/javase/model/Transactions.java
@@ -1,4 +1,13 @@
 package com.codecoolbank.javase.model;
 
+import java.math.BigDecimal;
+
 public class Transactions {
+    private String dateOfTransaction;
+    private BigDecimal value;
+    private String description;
+    private TransactionTypes transactionType;
+    private TransactionStatuses transactionStatus;
+    private Account account;
+    private Card card;
 }
