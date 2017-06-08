0f9a854691621e2b692d7e24acebb5d94be31cae
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:47:03 +0200

    Add constructor without ID in CardTypes class

--- src/main/java/com/codecoolbank/javase/model/CardTypes.java
+++ src/main/java/com/codecoolbank/javase/model/CardTypes.java
@@ -1,4 +1,8 @@
 package com.codecoolbank.javase.model;
 
-public class CardTypes {
+public class CardTypes extends CodecoolBankModel {
+
+    public CardTypes(String name, String description) {
+        super(name, description);
+    }
 }
