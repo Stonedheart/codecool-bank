b78ef8b1b8fbd0d002b314e2c2843007a4012697
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 03:30:12 +0200

    Add new testIfConstructorWithoutIDIsInitializedValid in TransactionTypesTest class

--- src/test/java/com/codecoolbank/javase/model/TransactionTypesTest.java
+++ src/test/java/com/codecoolbank/javase/model/TransactionTypesTest.java
@@ -1,7 +1,13 @@
 package com.codecoolbank.javase.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionTypesTest {
-
+    @Test
+    void testIfConstructorWithoutIDIsInitializedValid() {
+        TransactionTypes accountStatuses = new TransactionTypes("External","External transaction to the another account");
+        assertEquals(TransactionTypes.class, accountStatuses.getClass());
+    }
 }
