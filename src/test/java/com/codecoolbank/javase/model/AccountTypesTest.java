114872c7ba83a007dfde8b2b8cf9303474481b8e
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:44:48 +0200

    Change values of fields passed to constructors in AccountStatusesTest class

--- src/test/java/com/codecoolbank/javase/model/AccountTypesTest.java
+++ src/test/java/com/codecoolbank/javase/model/AccountTypesTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class AccountTypesTest {
     @Test
     void testIfConstructorWithoutIDIsInitializedValid() {
-        AccountTypes accountStatuses = new AccountTypes("Debit","Account for your savings, without credit");
+        AccountTypes accountStatuses = new AccountTypes("Saving account","Account for your savings, percentage 2,5%");
         assertEquals(AccountTypes.class, accountStatuses.getClass());
     }
 
     @Test
     void testIfConstructorIsInitializedValid() {
-        AccountTypes accountStatuses = new AccountTypes(1,"Debit","Account for your savings, without credit");
+        AccountTypes accountStatuses = new AccountTypes(1,"Saving account","Account for your savings, percentage 2,5%");
         assertEquals(AccountTypes.class, accountStatuses.getClass());
     }
 }
