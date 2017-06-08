269f01d08c521c10f191daa218b92b731d0a3dd1
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 16:49:40 +0200

    Add test for checking if constructor with ID is initialized properly in DebitAccountTest.

--- src/test/java/com/codecoolbank/javase/model/DebitAccountTest.java
+++ src/test/java/com/codecoolbank/javase/model/DebitAccountTest.java
@@ -1,12 +1,14 @@
 package com.codecoolbank.javase.model;
 
 import org.junit.jupiter.api.Test;
+import org.omg.CORBA.DynAnyPackage.InvalidValue;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DebitAccountTest {
+
     @Test
     void testIfConstructorWithoutIDIsInitializedValid() {
         Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
@@ -14,6 +16,16 @@ class DebitAccountTest {
         AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
 
         DebitAccount debitAccount = new DebitAccount(customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        assertEquals(DebitAccount.class, debitAccount.getClass());
+    }
+
+    @Test
+    void testIfConstructorIsInitializedValid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        DebitAccount debitAccount = new DebitAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
         assertEquals(DebitAccount.class, debitAccount.getClass());
     }
 }
