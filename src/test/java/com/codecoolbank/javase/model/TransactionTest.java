fbd7275e7a80124a50c3ce5b68cb69827797111a
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 09:03:47 +0200

    Replace object from Account to AccountTest

--- src/test/java/com/codecoolbank/javase/model/TransactionTest.java
+++ src/test/java/com/codecoolbank/javase/model/TransactionTest.java
@@ -15,7 +15,7 @@ class TransactionTest {
         Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
         AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 2,5%");
         AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
-        Account account = new Account(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 1);
+        SavingAccount account = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 1);
 
         CardType cardType = new CardType(1, "Debit", "Debit card for your savings, without credit limit");
         CardStatus cardStatus = new CardStatus(1, "Active", "Card is active");
@@ -33,7 +33,7 @@ class TransactionTest {
         Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
         AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 2,5%");
         AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
-        Account account = new Account(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 1);
+        SavingAccount account = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 1);
 
         CardType cardType = new CardType(1, "Debit", "Debit card for your savings, without credit limit");
         CardStatus cardStatus = new CardStatus(1, "Active", "Card is active");
