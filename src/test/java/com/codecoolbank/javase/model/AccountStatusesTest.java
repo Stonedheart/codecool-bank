513b66f20911cad824726db8ef14a7a1a14b7682
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:17:33 +0200

    Add test for valid constructor with ID in AccountStatusesTest

--- src/test/java/com/codecoolbank/javase/model/AccountStatusesTest.java
+++ src/test/java/com/codecoolbank/javase/model/AccountStatusesTest.java
@@ -1,7 +1,13 @@
 package com.codecoolbank.javase.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AccountStatusesTest {
-
+    @Test
+    void testIfConstructorOfClassIsInitializedValid() {
+        AccountStatuses accountStatuses = new AccountStatuses(1,"Active","Account is active");
+        assertEquals(AccountStatuses.class, accountStatuses.getClass());
+    }
 }
