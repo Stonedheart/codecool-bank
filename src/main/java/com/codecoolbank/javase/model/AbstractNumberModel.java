c0aa78703ad2db83b0a851ed96575389d81d1de3
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 05:06:38 +0200

    Add new constructor without ID in AbstractNumberModel

--- src/main/java/com/codecoolbank/javase/model/AbstractNumberModel.java
+++ src/main/java/com/codecoolbank/javase/model/AbstractNumberModel.java
@@ -1,5 +1,9 @@
 package com.codecoolbank.javase.model;
 
-public abstract class AbstractNumberModel {
-    
+public abstract class AbstractNumberModel extends AbstractBaseModel {
+    private String number;
+
+    public AbstractNumberModel(String number) {
+        this.number = number;
+    }
 }
