7b2232675ecd22fe2a238a6df83125a1efa2a715
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 15:55:32 +0200

    Change name and object for method save Account to database in AccountDao interface

--- src/main/java/com/codecoolbank/javase/dao/AccountDao.java
+++ src/main/java/com/codecoolbank/javase/dao/AccountDao.java
@@ -1,12 +1,11 @@
 package com.codecoolbank.javase.dao;
 
 import com.codecoolbank.javase.model.Account;
-import com.codecoolbank.javase.model.Customer;
 import org.omg.CORBA.DynAnyPackage.InvalidValue;
 
 import java.sql.SQLException;
 
 public interface AccountDao {
-    void saveCustomerToDb(Customer customer) throws SQLException;
+    void saveAccountToDb(Account account) throws SQLException;
     Account findAccountByID(Integer id) throws SQLException, InvalidValue;
 }
