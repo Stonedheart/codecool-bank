8f4c1f24b52e113d5d04987237e58911f2350dcc
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 18:01:47 +0200

    Add tests to DebitAccountTest and SavingAccountTest classes.

--- src/test/java/com/codecoolbank/javase/model/SavingAccountTest.java
+++ src/test/java/com/codecoolbank/javase/model/SavingAccountTest.java
@@ -77,4 +77,15 @@ class SavingAccountTest {
         SavingAccount savingAccount = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
         assertEquals(BigDecimal.valueOf(2500.00), savingAccount.getBalance());
     }
+
+    @Test
+    void testIfSetBalanceLT0IsInvalid() throws InvalidValue {
+        Customer customer = new Customer(1,"Jan", "Kowalski", "JFK", "notDead", "11-11-2014", true, "24-01-2017");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        SavingAccount savingAccount = new SavingAccount(1, customer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+        BigDecimal newBalance = BigDecimal.valueOf(-1.00);
+        assertThrows(InvalidValue.class, ()-> new SavingAccount(savingAccount.getId(), savingAccount.getCustomer(), savingAccount.getNumber(), savingAccount.getAccountType(), savingAccount.getAccountStatus(), savingAccount.getOpenDate(), newBalance, savingAccount.getDebitLine(), savingAccount.getInterest()));
+    }
 }
