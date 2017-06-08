85ecb9e70742342e0ca4e84c4aa8a3aad586ada5
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 05:33:33 +0200

    Change instance name in CardTypeTest class

--- src/test/java/com/codecoolbank/javase/model/CardTypeTest.java
+++ src/test/java/com/codecoolbank/javase/model/CardTypeTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class CardTypeTest {
     @Test
     void testIfConstructorWithoutIDIsInitializedValid() {
-        CardType accountStatuses = new CardType("Debit", "Debit card for your savings, without credit limit");
-        assertEquals(CardType.class, accountStatuses.getClass());
+        CardType testCardType = new CardType("Debit", "Debit card for your savings, without credit limit");
+        assertEquals(CardType.class, testCardType.getClass());
     }
 
     @Test
     void testIfConstructorIsInitializedValid() {
-        CardType accountStatuses = new CardType(1, "Debit", "Debit card for your savings, without credit limit");
-        assertEquals(CardType.class, accountStatuses.getClass());
+        CardType testCardType = new CardType(1, "Debit", "Debit card for your savings, without credit limit");
+        assertEquals(CardType.class, testCardType.getClass());
     }
 }
