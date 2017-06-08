884383a19ec5e7e3bcf180a2c5b2fff341c4d084
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 13:04:38 +0200

    Rename method in AccountDao interface

--- src/main/java/com/codecoolbank/javase/dao/AccountDao.java
+++ src/main/java/com/codecoolbank/javase/dao/AccountDao.java
@@ -1,4 +1,10 @@
 package com.codecoolbank.javase.dao;
 
+import com.codecoolbank.javase.model.Account;
+import org.omg.CORBA.DynAnyPackage.InvalidValue;
+
+import java.sql.SQLException;
+
 public interface AccountDao {
+    Account findByID(Integer id) throws SQLException, InvalidValue;
 }
