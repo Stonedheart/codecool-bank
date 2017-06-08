90907f98d5cd94d6db945cbb2dba9db7b4eca703
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 04:40:27 +0200

    Declare private fields in Accounts class

--- src/main/java/com/codecoolbank/javase/model/Accounts.java
+++ src/main/java/com/codecoolbank/javase/model/Accounts.java
@@ -1,4 +1,14 @@
 package com.codecoolbank.javase.model;
 
+import java.math.BigDecimal;
+
 public class Accounts {
+    private Customer customer;
+    private String number;
+    private AccountTypes accountType;
+    private AccountStatuses accountStatus;
+    private String openDate;
+    private BigDecimal balance;
+    private BigDecimal debitLine;
+    private Integer interest;
 }
