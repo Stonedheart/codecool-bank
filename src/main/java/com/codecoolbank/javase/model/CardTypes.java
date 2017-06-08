2570c7c60c44d9ac397de3603c44f2aa8d9d0fd2
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 02:47:34 +0200

    Add constructor with ID in CardTypes class

--- src/main/java/com/codecoolbank/javase/model/CardTypes.java
+++ src/main/java/com/codecoolbank/javase/model/CardTypes.java
@@ -5,4 +5,8 @@ public class CardTypes extends CodecoolBankModel {
     public CardTypes(String name, String description) {
         super(name, description);
     }
+
+    public CardTypes(Integer id, String name, String description) {
+        super(id, name, description);
+    }
 }
