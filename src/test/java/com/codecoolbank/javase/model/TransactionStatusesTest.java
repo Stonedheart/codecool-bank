cea7fb97725c40ee9e5d3314d4a7802187fe4c11
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-08 03:41:34 +0200

    Add new testIfConstructorWithoutIDIsInitializedValid in TransactionStatusesTest class

--- src/test/java/com/codecoolbank/javase/model/TransactionStatusesTest.java
+++ src/test/java/com/codecoolbank/javase/model/TransactionStatusesTest.java
@@ -1,7 +1,13 @@
 package com.codecoolbank.javase.model;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TransactionStatusesTest {
-
+    @Test
+    void testIfConstructorWithoutIDIsInitializedValid() {
+        TransactionStatuses accountStatuses = new TransactionStatuses("Waiting","Your transaction is in order to accomplish");
+        assertEquals(TransactionStatuses.class, accountStatuses.getClass());
+    }
 }
