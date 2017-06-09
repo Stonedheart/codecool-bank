fe30b4706f805f4dbd2f602c430fa3ff48fce02b
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-09 04:54:32 +0200

    Test if updating SavingAccount in database in AccountDaoImpl works properly

--- src/test/java/com/codecoolbank/javase/dao/AccountDaoImplTest.java
+++ src/test/java/com/codecoolbank/javase/dao/AccountDaoImplTest.java
@@ -11,7 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AccountDaoImplTest {
     @Test
-    void testInsertSavingAccountToDbInsertQuery() throws SQLException, InvalidValue {
+    void testSavingAccountToDbByUpdateQuery() throws SQLException, InvalidValue {
         AccountDaoImpl accountDaoImpl = new AccountDaoImpl();
         Customer testCustomer = new Customer("Marian", "Nowak", "mrn", "1234", "01-01-2012", true, "10-11-2016");
         AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
@@ -24,17 +24,16 @@ class AccountDaoImplTest {
         assertEquals(testCustomerId, accountDaoImpl.findAccountByID(testCustomerId).getId());
     }
 
-    @Test
-    void testInsertDebitAccountToDbInsertQuery() throws SQLException, InvalidValue {
-        AccountDaoImpl accountDaoImpl = new AccountDaoImpl();
-        Customer testCustomer = new Customer("Marian", "Nowak", "mrn", "1234", "01-01-2012", true, "10-11-2016");
-        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
-        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
-
-        DebitAccount debitAccount = new DebitAccount(1, testCustomer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
-
-        accountDaoImpl.saveAccountToDb(debitAccount);
-        Integer testCustomerId = 2;
-        assertEquals(testCustomerId, accountDaoImpl.findAccountByID(testCustomerId).getId());
-    }
+//    @Test
+//    void testIfFindByIdIsValid() throws SQLException {
+//        Customer customerFromDb = new CustomerDaoImpl().findCustomerById(1);
+//        assertTrue(customerFromDb.getId() == 1);
+//    }
+//
+//    @Test
+//    void testIfFindByIdReturnNullIfIdIsInvalid() throws SQLException {
+//        Integer invalidCustomerId = 10000000;
+//        Customer customerFromDb = new CustomerDaoImpl().findCustomerById(invalidCustomerId);
+//            assertEquals(null, customerFromDb );
+//    }
 }
