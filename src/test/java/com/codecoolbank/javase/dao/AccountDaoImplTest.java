982bece22c5d74c22be8bd17436954f8b9e7a093
Tomasz M. Krystyan <neo.dark.lord@gmail.com>
2017-06-09 04:29:35 +0200

    Test if insert SavingAccount and DebitAccount to database in AccountDaoImpl works properly

---
+++ src/test/java/com/codecoolbank/javase/dao/AccountDaoImplTest.java
@@ -0,0 +1,40 @@
+package com.codecoolbank.javase.dao;
+
+import com.codecoolbank.javase.model.*;
+import org.junit.jupiter.api.Test;
+import org.omg.CORBA.DynAnyPackage.InvalidValue;
+
+import java.math.BigDecimal;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDaoImplTest {
+    @Test
+    void testInsertSavingAccountToDbInsertQuery() throws SQLException, InvalidValue {
+        AccountDaoImpl accountDaoImpl = new AccountDaoImpl();
+        Customer testCustomer = new Customer("Marian", "Nowak", "mrn", "1234", "01-01-2012", true, "10-11-2016");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        SavingAccount savingAccount = new SavingAccount(1, testCustomer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+
+        accountDaoImpl.saveAccountToDb(savingAccount);
+        Integer testCustomerId = 2;
+        assertEquals(testCustomerId, accountDaoImpl.findAccountByID(testCustomerId).getId());
+    }
+
+    @Test
+    void testInsertDebitAccountToDbInsertQuery() throws SQLException, InvalidValue {
+        AccountDaoImpl accountDaoImpl = new AccountDaoImpl();
+        Customer testCustomer = new Customer("Marian", "Nowak", "mrn", "1234", "01-01-2012", true, "10-11-2016");
+        AccountType accountType = new AccountType(1, "Saving account", "Account for your savings, percentage 5%");
+        AccountStatus accountStatus = new AccountStatus(1, "Active", "Account is active");
+
+        DebitAccount debitAccount = new DebitAccount(1, testCustomer, "00008798123400000012", accountType, accountStatus, "12-02-2015", BigDecimal.valueOf(2500.00), BigDecimal.valueOf(0.00), 5);
+
+        accountDaoImpl.saveAccountToDb(debitAccount);
+        Integer testCustomerId = 2;
+        assertEquals(testCustomerId, accountDaoImpl.findAccountByID(testCustomerId).getId());
+    }
+}
